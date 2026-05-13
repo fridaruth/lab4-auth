0c3012a2a8e1c9f12c13d0e83f9c8bc8a909f9f3
Frida <fridapihlstrom@gmail.com>
2026-05-13 15:37:16 +0200

    Server och authentication routes

---
+++ routes/authRoutes.js
@@ -0,0 +1,15 @@
+/**
+ * routes for auth
+ */
+const express = require("express");
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+    console.log("register called...");
+});
+
+router.post("/login", async(req, res) => {
+    console.log("login called...");
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+ * Autentisering och inloggning
+ */
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+
+// routes
+app.use("/api", authRoutes);
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+});
