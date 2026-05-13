ba1cdd08cb1babd6e3ee975fc4bed022879c46f2
Frida <fridapihlstrom@gmail.com>
2026-05-13 16:01:46 +0200

    Lagt till modell för användare och konfiguration för MongoDB anslutning

--- server.js
+++ server.js
@@ -5,11 +5,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
+
+// mongoDB
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Ansluten till MongoDB!");
+    })
+    .catch((err) => {
+        console.error("Kunde inte ansluta till databasen:", err);
+    });
 
 // routes
 app.use("/api", authRoutes);
