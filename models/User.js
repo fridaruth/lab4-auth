b9a59d50d603ec076ba3092d1fadfd7d6cd7eea6
Frida <fridapihlstrom@gmail.com>
2026-05-18 11:26:50 +0200

    implementera inloggning och lösenordshantering

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -17,4 +18,59 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model("User", userSchema);
+// hash password
+userSchema.pre("save", async function () {
+        if (this.isNew || this.isModified("password")) {
+            const hashPassword = await bcrypt.hash(this.password, 10);
+            this.password = hashPassword;
+        }
+    });
+
+// register user
+userSchema.statics.register = async function (username, password) {
+    try {
+        const user = new this({ username, password });
+        await user.save();
+        return user;
+    } catch (error) {
+        throw error;
+    }
+};
+
+// compare hashed passwords
+userSchema.methods.comparePassword = async function (password) {
+    try {
+        return await bcrypt.compare(password, this.password);
+    } catch (error) {
+        throw error;
+    }
+
+
+}
+
+// login
+userSchema.statics.login = async function (username, password) {
+    try {
+        const user = await this.findOne({ username });
+
+        if (!user) {
+            throw new Error("incorrect username/password")
+        }
+
+        const isPasswordMatch = await user.comparePassword(password);
+
+        // incorrect?
+        if (!isPasswordMatch) {
+            throw new Error("incorrect username/password");
+        }
+
+        // correct
+        return user;
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,10 +2,13 @@
  * routes for auth
  */
 
+const jwt = require("jsonwebtoken");
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 router.post("/register", async (req, res) => {
     try {
@@ -16,25 +19,54 @@ router.post("/register", async (req, res) => {
             return res.status(400).json({ error: "Användarnamn och lösenord krävs!"});
         }
 
-        // kryptera lösenord
-        const hashPassword = await bcrypt.hash(password, 10);
-
         // skapa och spara användare
         const user = new User({
             username: username,
-            password: hashPassword
+            password: password
         });
-
         await user.save();
 
         res.status(201).json({ message: "Användare skapad!" })
     } catch (error) {
-        res._construct.status(500).json({ error: "Kunde inte registrera användare!" })
+        console.error(error);
+        res.status(500).json({ error: "Kunde inte registrera användare!" })
     }
 });
 
 router.post("/login", async(req, res) => {
-    console.log("login called...");
+    try {
+        const { username, password } = req.body;
+
+        // kontrollera input
+        if (!username || !password ) {
+            return res.status(400).json({ error: "Användarnamn och lösenord krävs" });
+        }
+
+        // kolla om användare finns
+        const user = await User.findOne({ username });
+        if(!user) {
+            return res.status(401).json({ error: "Felaktigt användarnamn eller lösenord" });
+        }
+
+        // kontrollera lösenord
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(401).json({ error: "Felaktigt användarnamn eller lösenord" });
+        }
+
+        // skapa JWT-token
+        const payload = { username: user.username };
+        const secretKey = process.env.JWT_SECRET_KEY;
+        const token = jwt.sign(payload, secretKey, { expiresIn: "1h" });
+
+        res.status(200).json({
+            message: "Inloggning lyckades!",
+            token: token
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Serverfel vid inloggning" });
+    }
 });
 
 module.exports = router;
