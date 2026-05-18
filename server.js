73e304fcefb66ccdbf81c0529e8ccd02ed8dba34
Frida <fridapihlstrom@gmail.com>
2026-05-18 13:45:30 +0200

    Lägg till CORS-stöd

--- server.js
+++ server.js
@@ -4,12 +4,15 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 app.use(bodyParser.json());
 
 // mongoDB
